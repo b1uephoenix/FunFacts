4c47c419c9704116fb00daf7246ca081d0076e50
b1uephoenix <pjriverotter.av@gmail.com>
2014-12-02 18:53:30 -0800

    created FactBook class

---
+++ app/src/main/java/com/teamtreehouse/funfacts/FactBook.java
@@ -0,0 +1,46 @@
+package com.teamtreehouse.funfacts;
+
+import java.util.Random;
+
+/**
+ * Created by Philip on 12/2/2014.
+ */
+public class FactBook {
+
+    // Member variable (properties about the object)
+
+    // Method (abilities: things the object can do)
+    public String getFact() {
+
+        String[] facts = {
+                "Ants stretch when they wake up in the morning.",
+                "Ostriches can run faster than horses.",
+                "Olympic gold medals are actually made mostly of silver.",
+                "You are born with 300 bones; by the time you are an adult you will have 206.",
+                "It takes about 8 minutes for light from the Sun to reach Earth.",
+                "Some bamboo plants can grow almost a meter in just one day.",
+                "The state of Florida is bigger than England.",
+                "Some penguins can leap 2-3 meters out of the water.",
+                "On average, it takes 66 days to form a new habit.",
+                "Mammoths still walked the earth when the Great Pyramid was being built.",
+                "Treehouse is not actually in a tree.",
+                "Yuffie is a poofie.",
+                "Yuffie goes meow meow.",
+                "The Monkey climbs the tree and eats the banana.",
+                "Flies on the wall eat like a ball.",
+                "Cows in the house sleep like a mouse."};
+
+        //The button was clicked, so update the fact label with a new fact
+        String fact = "";
+
+
+        //Randomly select a fact
+        //use NEW to construct a new class (Random class)
+        Random randomGenerator = new Random(); //Construct a new random number generator assign the variable randomGenerator
+        int randomNumber = randomGenerator.nextInt(facts.length);
+
+        fact = facts[randomNumber];
+
+        return fact;
+    }
+}
