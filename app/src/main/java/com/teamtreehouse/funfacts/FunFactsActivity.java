0a6f3f0440f2df9165eb4f147d9f6e1e63bab233
b1uephoenix <pjriverotter.av@gmail.com>
2014-12-02 19:16:06 -0800

    created FactBook member variable

--- app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
@@ -13,6 +13,8 @@ import java.util.Random;
 
 public class FunFactsActivity extends Activity {
 
+    private FactBook mFactBook = new FactBook();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,57 +28,13 @@ public class FunFactsActivity extends Activity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String[] facts = {
-                "Ants stretch when they wake up in the morning.",
-                        "Ostriches can run faster than horses.",
-                        "Olympic gold medals are actually made mostly of silver.",
-                        "You are born with 300 bones; by the time you are an adult you will have 206.",
-                        "It takes about 8 minutes for light from the Sun to reach Earth.",
-                        "Some bamboo plants can grow almost a meter in just one day.",
-                        "The state of Florida is bigger than England.",
-                        "Some penguins can leap 2-3 meters out of the water.",
-                        "On average, it takes 66 days to form a new habit.",
-                        "Mammoths still walked the earth when the Great Pyramid was being built.",
-                        "Treehouse is not actually in a tree.",
-                        "Yuffie is a poofie.",
-                        "Yuffie goes meow meow.",
-                        "The Monkey climbs the tree and eats the banana.",
-                        "Flies on the wall eat like a ball.",
-                        "Cows in the house sleep like a mouse."};
-
-                //The button was clicked, so update the fact label with a new fact
-                String fact = "";
-                //Randomly select a fact
-                //use NEW to construct a new class (Random class)
-                Random randomGenerator = new Random(); //Construct a new random number generator assign the variable randomGenerator
-                int randomNumber = randomGenerator.nextInt(facts.length);
-                fact = facts[randomNumber];
+                    String fact = mFactBook.getFact();
                 //Update the label with our dynamic fact
                 factLabel.setText(fact);
-
 
             }
         };
         showFactButton.setOnClickListener(listener);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.fun_facts, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
